6b3065d4d0b02746dc022c862e4d767f48095be3
Jorgetorreslopez <usda4ever@gmail.com>
2023-04-30 21:10:46 -0400

    Created first Screen Map & Player Sprite

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('APP IS CONNECTED')
+/*----- constants -----*/
+const player = document.getElementById("player");
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
