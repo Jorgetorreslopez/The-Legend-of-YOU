7c7e4b0d82589e8606092eb3b58e3090076e3511
Jorgetorreslopez <usda4ever@gmail.com>
2023-05-01 22:33:25 -0400

    Created Movement Functionality For Player

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 console.log('APP IS CONNECTED')
 /*----- constants -----*/
-const player = document.getElementById("player");
+let gameDiv = document.getElementById('playWindow')
+let charDiv = document.getElementById('player')
+console.log(charDiv)
+
 
 /*----- state variables -----*/
 
@@ -10,5 +13,32 @@ const player = document.getElementById("player");
 
 /*----- event listeners -----*/
 
+document.addEventListener('keydown', handleKeys)
+var charLeftAdd = 500
+var charTopAdd = 400
 
 /*----- functions -----*/
+
+function handleKeys(e) {
+    e.preventDefault()
+    let keyPress = e.code;
+    if (keyPress === 'ArrowRight') {
+      charLeftAdd += 10;  
+      charDiv.style.left = charLeftAdd + 'px';
+    }
+
+    if (keyPress === 'ArrowLeft') {
+      charLeftAdd -= 10;
+      charDiv.style.left = charLeftAdd + 'px';
+    }
+
+    if (keyPress === 'ArrowDown') {
+      charTopAdd += 10;
+      charDiv.style.top = charTopAdd + 'px';
+    }
+
+    if (keyPress === 'ArrowUp') {
+      charTopAdd -= 10;
+      charDiv.style.top = charTopAdd + 'px';
+    }
+}
