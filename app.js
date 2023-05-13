deaff1860e4b5746736ee69e4e54379edb2f2c65
Jorgetorreslopez <usda4ever@gmail.com>
2023-05-13 00:22:45 -0400

    Created Intro Screen & finised Game

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ let charTopPosition = 400;
 
 let enemyHitpoints = 6;
 
+// let timeLimit = 5
+
 /*----- cached elements  -----*/
 
 childWallCollisionDivs.forEach((div) => {
@@ -45,9 +47,7 @@ function handleKeys(e) {
 
   if (keydown === "ArrowRight") {
     console.log(wallCollision(0, 0, 10, 0) || grabSword(0, 0, 10, 0));
-
-    if (!wallCollision(0, 0, 10, 0)) {
-      grabSword(0, 0, 10, 0);
+    if (!wallCollision(0, 0, 10, 0) || grabSword(0, 0, 10, 0)) {
       charLeftPosition += 10;
     }
     if (charLeftPosition + charDiv.offsetWidth <= gameDiv.offsetWidth) {
@@ -59,9 +59,7 @@ function handleKeys(e) {
 
   if (keydown === "ArrowLeft") {
     console.log(wallCollision(0, -10, 0, 0) || grabSword(0, -10, 0, 0));
-
-    if (!wallCollision(0, -10, 0, 0)) {
-      grabSword(0, -10, 0, 0);
+    if (!wallCollision(0, -10, 0, 0) || grabSword(0, -10, 0, 0)) {
       charLeftPosition -= 10;
     }
     if (charLeftPosition >= 0) {
@@ -73,9 +71,7 @@ function handleKeys(e) {
 
   if (keydown === "ArrowDown") {
     console.log(wallCollision(0, 0, 0, 10) || grabSword(0, 0, 0, 10));
-
-    if (!wallCollision(0, 0, 0, 10)) {
-      grabSword(0, 0, 0, 10);
+    if (!wallCollision(0, 0, 0, 10) || grabSword(0, 0, 0, 10)) {
       charTopPosition += 10;
     }
     if (charTopPosition + charDiv.offsetHeight <= gameDiv.offsetHeight) {
@@ -91,9 +87,11 @@ function handleKeys(e) {
         grabSword(-10, 0, 0, 0) ||
         detectDoor(-10, 0, 0, 0)
     );
-
-    if (!wallCollision(-10, 0, 0, 0) || detectDoor(-10, 0, 0, 0)) {
-      grabSword(-10, 0, 0, 0);
+    if (
+      !wallCollision(-10, 0, 0, 0) ||
+      grabSword(-10, 0, 0, 0) ||
+      detectDoor(-10, 0, 0, 0)
+    ) {
       charTopPosition -= 10;
     }
     if (charTopPosition >= 0) {
@@ -182,10 +180,9 @@ function grabSword(top, left, right, bottom) {
     charTop + top < swordRect.bottom &&
     charTop + charDiv.offsetHeight + bottom > swordRect.top;
   console.log("begin", grabbedSwordTurn);
-
   if (overlapSwordX && overlapSwordY) {
-    if (grabbedSwordTurn === 0) {
-      grabbedSwordTurn++;
+    grabbedSwordTurn = 1;
+    if (grabbedSwordTurn === 1) {
       if (document.getElementById("map2")) {
         hasSword = true;
         document.getElementById("sword").style.display = "none";
@@ -193,13 +190,8 @@ function grabSword(top, left, right, bottom) {
         const enemyPosition = document.querySelector(".darkside");
         enemyPosition.style.top = "400px";
         enemyPosition.style.left = "400px";
-        return true;
-      }
-    } else if (grabbedSwordTurn === 2) {
-      console.log("increasingswordTurn");
 
-      setTimeout(() => {
-        let timeLimit = 15;
+        let timeLimit = 30;
         let timer = setInterval(() => {
           timeLimit--;
           console.log(timeLimit);
@@ -208,7 +200,8 @@ function grabSword(top, left, right, bottom) {
             lossMessage();
           }
         }, 1000);
-      }, 0);
+        return true;
+      }
     }
   }
 }
@@ -243,21 +236,28 @@ function attackEnemy(top, left, right, bottom) {
 
   if (overlapEnemyX && overlapEnemyY && hasSword) {
     if (spaceKeyIsPressed) {
-      alert("Nani!!!");
+      alert(
+        "No matter how much you hit me, I'll keep saying it. Laura Bush Killed A guy."
+      );
       enemyHitpoints--;
 
       if (enemyHitpoints === 0) {
         enemy.style.display = "none";
         const enemyDefeated = document.createElement("div");
-        enemyDefeated.textContent = "Yo You Did it!!";
+        enemyDefeated.textContent =
+          "You did it!! Now here's a random guy to congratulate you.";
         enemyDefeated.style.fontFamily = "Press Start 2P";
         enemyDefeated.style.fontSize = "2rem";
         enemyDefeated.style.color = "goldenrod";
         enemyDefeated.style.textAlign = "center";
         enemyDefeated.style.position = "absolute";
         enemyDefeated.style.top = "150px";
-        enemyDefeated.style.left = "830px";
+        enemyDefeated.style.left = "565px";
         document.body.appendChild(enemyDefeated);
+
+        setTimeout(() => {
+          window.location.href = "https://www.youtube.com/watch?v=1Bix44C1EzY";
+        }, 5000);
       }
     }
   }
@@ -287,7 +287,7 @@ function takingDamage() {
   if (directHit) {
     dropSword();
     alert(
-      "lol..You died in the the first couple of seconds... I've seen worst honestly but look we don't always get it on the first try. As long as you continue to pick up your reason, You can keep trying. This time after picking up your reason, press the the Space key and time it just right. Remember, the the longer you wait to take action, lets say maybe more than 6 seconds, the more likely you'll fail and lose sight of your reason. By the way did you notice the the double the's in this message? Good luck Loser, love you and be safe."
+      "lol..You died in the the first couple of seconds... I've seen worst honestly but look we don't always get it on the first try. As long as you continue to pick up your reason, You can keep trying. This time after picking up your reason, press the the Space key and time it just right. Remember, the the longer you wait to take action, the more likely you'll fail and lose sight of your reason. So you have 20 seconds to pick your sword back up and defeat your doubts. By the way did you notice the the double the's in this message? Good luck Loser, love you and be safe."
     );
   } else {
     requestAnimationFrame(takingDamage);
@@ -311,17 +311,40 @@ takingDamage();
 function lossMessage() {
   const youLose = document.createElement("div");
   youLose.innerHTML =
-    '<p>You Suck</p> <button onclick="redirectToURL()">Rolled</button>';
+    '<p>You took too long, but do not worry. Even procrastination deserves a prize. Perhaps this game sucks & that is why you lost? Click below.</p> <button onclick="redirectToURL()">It does suck. Please give me my prize.</button>';
   youLose.style.position = "fixed";
   youLose.style.top = "50%";
   youLose.style.left = "50%";
   youLose.style.transform = "translate(-50%, -50%)";
-  youLose.style.background = "white";
+  youLose.style.background = "black";
+  youLose.style.color = "white";
   youLose.style.padding = "20px";
   youLose.style.textAlign = "center";
+  youLose.style.fontFamily = "Courier, monospace";
+  youLose.style.fontSize = "1.5rem";
+  youLose.style.border = "5px solid white";
+  youLose.style.borderRadius = "10px";
+  youLose.style.boxShadow = "0px 0px 10px 2px rgba(255, 255, 255, 0.3)";
   document.body.appendChild(youLose);
 }
 
 function redirectToURL() {
-  window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+  if (document.getElementById("map2")) {
+    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+  } else {
+    const step2Message = document.createElement("div");
+    step2Message.textContent = "STEP 2. MAKE IT THE RIGHT DECISION";
+    step2Message.style.fontFamily = "Press Start 2P cursive";
+    step2Message.style.fontSize = "2rem";
+    step2Message.style.color = "goldenrod";
+    step2Message.style.textAlign = "center";
+    step2Message.style.position = "absolute";
+    step2Message.style.top = "750px";
+    step2Message.style.left = "650px";
+    document.body.appendChild(step2Message)
+    setTimeout(() => {
+      window.location.href =
+        "file:///Users/jorgetorreslopez/portfolio-projects/projects/unit-1-project/index.html";
+    }, 5000);
+  }
 }
